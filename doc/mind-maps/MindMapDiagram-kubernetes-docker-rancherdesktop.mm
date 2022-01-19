6fc22640cb61c7cdc64099ccfb921dd2c9ea728d
Josemar Furegatti de Abreu Silva <josemarsilva@inmetrics.com.br>
2022-01-19 10:35:42 -0300

    doc: doc/mind-maps/MindMapDiagram-kubernetes-docker-rancherdesktop

--- doc/mind-maps/MindMapDiagram-kubernetes-docker-rancherdesktop.mm
+++ doc/mind-maps/MindMapDiagram-kubernetes-docker-rancherdesktop.mm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac72c8807e5949c3871ebd585059c78c0c84162c4689b8ca46343ad58fbb082e
-size 3557
+oid sha256:537b2fc5478e1b54ad3864acd5933f11fe623fc4ce055386efd4b58ebfcaf8ab
+size 3571
