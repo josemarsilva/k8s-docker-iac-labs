ecb38c973d66fb3c230769fb09e7a93b7199b6aa
Josemar Furegatti de Abreu Silva <josemarsilva@inmetrics.com.br>
2022-02-19 15:52:50 -0300

    feat: k8s-config-0.yaml e k8s-config-1.yaml

--- doc/mind-maps/MindMapDiagram-kubernetes-docker-rancherdesktop.mm
+++ doc/mind-maps/MindMapDiagram-kubernetes-docker-rancherdesktop.mm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5111013d344afe7db55daff77f9c3cd3284cea50ecefa37e8bee35f71fda786c
-size 15910
+oid sha256:bfe9ddc29c7fe102e0ee53ed2497b0efb7738d045143495294798c3d0f121395
+size 16757
